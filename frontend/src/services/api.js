ca9605f6a62ffb55b3b5989d18b1e52f507f1ee9
Karthick <ss.karthick61@gmail.com>
2025-08-26 19:00:09 +0530

    api updates

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,5 @@
-export const API_URL = import.meta.env.VITE_API_URL ;
+export const API_URL =
+  import.meta.env.VITE_API_URL ||
+  (window.location.host.includes('github.io')
+    ? 'https://login-backend-rulc.onrender.com'
+    : 'http://localhost:5000');
